cadb9ecd1e5bafd770458d5ac8f272095acee03a
mrpareek0603 <pareekrohan.rp@gmail.com>
2023-07-13 21:57:27 +0530

    second commit learnt about JSX and Functional components episode 3 over

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
-{
-  /* <div id="parent">
-    <div id = "child">
-        <h1>h1111 im inside child</h1>
-    </div>
-</div> */
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "h111 im inside child"),
-    React.createElement("h2", {}, "h2 im also inside child"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "h111 im inside child"),
-    React.createElement("h2", {}, "h2 im also inside child"),
-  ]),
-]);
+const Title = function() {
+    return <h1>JSX heading</h1>
+};
 
-// const heading = React.createElement("h1", { id: "h" }, "hi from ro");
+// Functional Component
+// A javascript function which returns a react element or JSX
+const Heading = () => (
+  // return (
+  <div>
+    <Title />
+    <h1>I am a functional component</h1>
+  </div>
+  // );
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
-console.log(heading, typeof heading);
+root.render(<Heading />);
+root.render(<Title />);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React
+
+# Parcel
+<!-- execute(npx) the parcel command (which is responsible for making the build) -->
+- npx parcel index.html
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - gives Faster Builds
+- Image Optimization
+- Minification of files
+- Core job - It bundles
+- Compress the files
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - supports older browsers
+- Diagnostics - of error
+- Error Handling
+- Tree Shaking - remove unused code
+- Different bundles for dev and prod
